470eba47f4c8b1d3eddf0f75c05071f8972bb2e1
Kane.Sun <taojintianxia@126.com>
2014-02-20 11:20:48 +0800

    add jars file and modified .gitignore file to upload jar file and avoid target folder to be uploaded

---
+++ src/test/TestBean.java
@@ -0,0 +1,41 @@
+package test;
+
+/**
+ * 
+ * 
+ * @author Kane.Sun
+ * @version Feb 19, 2014 3:22:54 PM
+ * 
+ */
+
+public class TestBean {
+
+	private int id = 123;
+	private String name = "test";
+	private int age = 18;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
