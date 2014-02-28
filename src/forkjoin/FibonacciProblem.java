4bb21ca5bc2c6be85b0d8c14cc6bbb2a02c1d551
Kane.Sun <taojintianxia@126.com>
2014-02-28 16:56:45 +0800

    add fork join example

---
+++ src/forkjoin/FibonacciProblem.java
@@ -0,0 +1,30 @@
+package forkjoin;
+
+/**
+ * 
+ * 
+ * @author Kane.Sun
+ * @version Feb 28, 2014 3:54:51 PM
+ * 
+ */
+public class FibonacciProblem {
+
+	public int n;
+
+	public FibonacciProblem(int n) {
+		this.n = n;
+	}
+
+	public long solve() {
+		return fibonacci(n);
+	}
+
+	private long fibonacci(int n) {
+		System.out.println("Thread: " + Thread.currentThread().getName() + " calculates " + n);
+		if (n <= 1)
+			return n;
+		else
+			return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
+}
