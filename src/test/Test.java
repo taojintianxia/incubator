27f86bd475cca592ebdf9ab28e9f068797d1b92e
Kane.Sun <taojintianxia@126.com>
2014-11-18 10:25:29 +0800

    update the test file , just a test

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,49 +2,7 @@ package test;
 
 public class Test {
 
-	public static void main(String... args) throws Exception {
-		Test test = new Test();
-		Test test2 = new Test();
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					test.methodA();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-
-		}).start();
-
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					test.methodB();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-
-		}).start();
+	public static void main(String... args) {
 
 	}
-
-	public synchronized void methodA() throws Exception {
-		for (int i = 0; i < 10; i++) {
-			System.out.println("A is calling");
-			Thread.sleep(200);
-		}
-	}
-
-	public synchronized void methodB() throws Exception {
-		for (int i = 0; i < 10; i++) {
-			System.out.println("B is calling");
-			Thread.sleep(200);
-		}
-	}
-
 }
