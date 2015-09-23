348c435d207eda1a3ed7f7dafbb49b37ba287f98
Kane.Sun <taojintianxia@126.com>
2015-09-23 11:45:09 +0800

    更改测试用例

--- src/main/java/test/JavaTest.java
+++ src/main/java/test/JavaTest.java
@@ -3,7 +3,7 @@ package test;
 public class JavaTest {
 	public static void main(String... args) {
 
-		JavaTest testclass = new JavaTest();
+		final JavaTest testclass = new JavaTest();
 
 		for (int i = 0; i < 50; i++) {
 			new Thread(new Runnable() {
