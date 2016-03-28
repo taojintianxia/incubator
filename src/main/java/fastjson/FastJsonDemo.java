bcc006996978072f11d834d548376c126920eca6
Kane.Sun <taojintianxia@126.com>
2016-03-28 13:49:48 +0800

    format code

--- src/main/java/fastjson/FastJsonDemo.java
+++ src/main/java/fastjson/FastJsonDemo.java
@@ -12,7 +12,7 @@ import model.User;
 
 @Slf4j
 public class FastJsonDemo {
-	
+
 //	public static final Object parse(String text); // 把JSON文本parse为JSONObject或者JSONArray 
 //	public static final JSONObject parseObject(String text)； // 把JSON文本parse成JSONObject    
 //	public static final  T parseObject(String text, Class clazz); // 把JSON文本parse为JavaBean 
@@ -21,29 +21,29 @@ public class FastJsonDemo {
 //	public static final String toJSONString(Object object); // 将JavaBean序列化为JSON文本 
 //	public static final String toJSONString(Object object, boolean prettyFormat); // 将JavaBean序列化为带格式的JSON文本 
 //	public static final Object toJSON(Object javaObject); 将JavaBean转换为JSONObject或者JSONArray。
-	
-	@Test
-	public void testJsonTransfer(){
-		User user = new User();
-		user.setAge(11);
-		user.setPassword("abc");
-		user.setUsername("kkk");
-		
-		Map<String, User> userMap = new HashMap<>();
-		userMap.put("kkk", user);
-		
-		log.info(JSON.toJSONString(userMap));
-	}
-	
-	@Test
-	public void parseWithSpecificClazz() {
-		String jsonStr = "{\"kkk\":{\"age\":11,\"password\":\"abc\",\"username\":\"kkk\"}}";
-		Map<String, String> userMap = new HashMap<>();
-		userMap = JSON.parseObject(jsonStr, userMap.getClass());
-		for (Object o : userMap.entrySet()) {
-			Map.Entry<String, Object> entry = (Map.Entry<String, Object>) o;
-			System.out.println(entry.getKey() + " : " + JSON.parseObject(entry.getValue().toString(),User.class).getUsername());
-		}
-	}
+
+    @Test
+    public void testJsonTransfer() {
+        User user = new User();
+        user.setAge(11);
+        user.setPassword("abc");
+        user.setUsername("kkk");
+
+        Map<String, User> userMap = new HashMap<>();
+        userMap.put("kkk", user);
+
+        log.info(JSON.toJSONString(userMap));
+    }
+
+    @Test
+    public void parseWithSpecificClazz() {
+        String jsonStr = "{\"kkk\":{\"age\":11,\"password\":\"abc\",\"username\":\"kkk\"}}";
+        Map<String, String> userMap = new HashMap<>();
+        userMap = JSON.parseObject(jsonStr, userMap.getClass());
+        for (Object o : userMap.entrySet()) {
+            Map.Entry<String, Object> entry = (Map.Entry<String, Object>) o;
+            System.out.println(entry.getKey() + " : " + JSON.parseObject(entry.getValue().toString(), User.class).getUsername());
+        }
+    }
 
 }
