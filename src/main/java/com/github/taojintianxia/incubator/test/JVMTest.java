c42dbf3578a3e1f22ef3413e38df39565a70b725
nianjun.sun <sunnianjun@rongcapital.cn>
2017-05-10 19:53:32 +0800

    更新关于H2的代码

--- src/main/java/com/github/taojintianxia/incubator/test/JVMTest.java
+++ src/main/java/com/github/taojintianxia/incubator/test/JVMTest.java
@@ -1,5 +1,12 @@
 package com.github.taojintianxia.incubator.test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 public class JVMTest {
     public static void main(String... args) {
         Runtime run = Runtime.getRuntime();
@@ -12,5 +19,27 @@ public class JVMTest {
         System.out.println("已分配内存 = " + total);
         System.out.println("已分配内存中的剩余空间 = " + free);
         System.out.println("最大可用内存 = " + usable);
+
+        Map<Integer, List<String>> testMap = new HashMap<>();
+        List<String> testList = new ArrayList<>();
+        testList.add(new String("A"));
+        testList.add(new String("B"));
+        testList.add(new String("C"));
+        testMap.put(1, testList);
+
+
+        String newString = testList.get(0);
+        testList.remove(0);
+        System.out.println("test list size is : " + testList.size());
+        System.out.println(newString);
+
+        Properties prop = System.getProperties();
+        Set<Object> keySet = prop.keySet();
+        for (Object obj : keySet) {
+            System.out.println("System Property: {" + obj.toString() + "," + System.getProperty(obj.toString()) + "}");
+        }
+
+
     }
+
 }
--- src/main/java/com/github/taojintianxia/incubator/test/JavaTest.java
+++ src/main/java/com/github/taojintianxia/incubator/test/JavaTest.java
@@ -1,37 +1,31 @@
 package com.github.taojintianxia.incubator.test;
 
 import java.util.Random;
-
-import com.google.common.base.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class JavaTest {
 
-    // public static void main(String... args) throws IOException {
-    // String grpc_plugin =
-    // "/Users/nianjun/Work/tools/grpc-java/compiler/build/exe/java_plugin/protoc-gen-grpc-java";
-    // String[] cmds =
-    // new String[] {"sh", "-c", "cd /Users/nianjun/Work/workspace/idl-transformer/idl/1487233348006
-    // ; "
-    // + "export PATH=$PATH:/usr/local/bin/ ; echo $PATH ; sh
-    // /Users/nianjun/Work/tools/grpc-java/compiler/compiler.sh ./helloworld.proto"};
-    //
-    // File file = new File("/Users/nianjun/Work/tools/grpc-java/compiler/compiler.sh");
-    // Process process = Runtime.getRuntime().exec(cmds);
-    // BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-    // String line = "";
-    // while ((line = input.readLine()) != null) {
-    // System.out.println(line);
-    // }
-    // }
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
 
-    public static void main(String... args) {
-        String nullableMaybe = null;
-        if (new Random().nextBoolean()) {
-            nullableMaybe = "xiba";
+        for (int i = 0; i < 30; i++) {
+            scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    long start = System.currentTimeMillis();
+                    try {
+                        Thread.sleep((new Random().nextInt(5) + 1) * 1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + " - Executed!");
+                    System.out.println("it takes " + (System.currentTimeMillis() - start) / 1000 + " seconds");
+                }
+            }, 0, 1, TimeUnit.SECONDS);
         }
-        String string = Optional.fromNullable(nullableMaybe).or("默认值");
-
-        System.out.println(string);
     }
+
 
 }
