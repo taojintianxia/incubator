607bd6e18199db90b9e42e34d2fbdce694f74d99
sunnianjun <sunnianjun@huobi.com>
2019-05-11 15:43:37 +0800

    Refactor: update the example

---
+++ incubator-netty/src/main/java/com/github/taojintianxia/incubator/netty/quickstart/timerserver/client/TimeDecoder.java
@@ -0,0 +1,19 @@
+package com.github.taojintianxia.incubator.netty.quickstart.timerserver.client;
+
+import com.github.taojintianxia.incubator.netty.quickstart.timerserver.client.pojo.UnixTime;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class TimeDecoder extends ByteToMessageDecoder { // (1)
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) { // (2)
+        if (in.readableBytes() < 4) {
+            return; // (3)
+        }
+
+        out.add(new UnixTime(in.readUnsignedInt()));
+    }
+}
