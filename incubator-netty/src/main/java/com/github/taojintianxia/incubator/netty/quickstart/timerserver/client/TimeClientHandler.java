e3a982793951928cc3be6aa322427e384268aa8f
sunnianjun <sunnianjun@huobi.com>
2019-05-11 15:44:40 +0800

    Refactor: refactor the channel read

--- incubator-netty/src/main/java/com/github/taojintianxia/incubator/netty/quickstart/timerserver/client/TimeClientHandler.java
+++ incubator-netty/src/main/java/com/github/taojintianxia/incubator/netty/quickstart/timerserver/client/TimeClientHandler.java
@@ -1,10 +1,8 @@
 package com.github.taojintianxia.incubator.netty.quickstart.timerserver.client;
 
-import io.netty.buffer.ByteBuf;
+import com.github.taojintianxia.incubator.netty.quickstart.timerserver.client.pojo.UnixTime;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-
-import java.util.Date;
 
 /**
  * @author Nianjun Sun
@@ -13,14 +11,9 @@ import java.util.Date;
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ByteBuf m = (ByteBuf) msg; // (1)
-        try {
-            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
-            System.out.println(new Date(currentTimeMillis));
-            ctx.close();
-        } finally {
-            m.release();
-        }
+        UnixTime m = (UnixTime) msg;
+        System.out.println(m);
+        ctx.close();
     }
 
     @Override
