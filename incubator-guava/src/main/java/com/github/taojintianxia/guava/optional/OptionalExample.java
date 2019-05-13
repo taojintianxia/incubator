6fca7b415f4d9921cc07741c88dfbfeafb7044e1
sunnianjun <sunnianjun@huobi.com>
2019-05-13 10:21:03 +0800

    Add: add optional example

---
+++ incubator-guava/src/main/java/com/github/taojintianxia/guava/optional/OptionalExample.java
@@ -0,0 +1,21 @@
+package com.github.taojintianxia.guava.optional;
+
+import com.google.common.base.Optional;
+
+/**
+ * @author Nianjun Sun
+ * @date 2019-05-13 10:17
+ */
+public class OptionalExample {
+
+    public static Optional<String> getNoneNull() {
+        Optional optionalStr = Optional.of(new String("optional test"));
+        return optionalStr;
+    }
+
+    public static void main(String... args) {
+        // 1. 这样的代码不需要NPE 检测，一定不会返回 null
+        Optional noneNullOptionalStr = getNoneNull();
+        System.out.println(noneNullOptionalStr.get());
+    }
+}
