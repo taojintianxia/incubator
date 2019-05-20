57944b3fd6f266ed0a202ec22fad94a83ab289f3
sunnianjun <sunnianjun@huobi.com>
2019-05-20 18:05:01 +0800

    Add: finished echo server

--- incubator-netty/src/main/java/com/github/taojintianxia/incubator/netty/inaction/chapter2/echo/EchoServer.java
+++ incubator-netty/src/main/java/com/github/taojintianxia/incubator/netty/inaction/chapter2/echo/EchoServer.java
@@ -1,5 +1,15 @@
 package com.github.taojintianxia.incubator.netty.inaction.chapter2.echo;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
 /**
  * @author Nianjun Sun
  * @date 2019-05-20 17:55
@@ -21,6 +31,21 @@ public class EchoServer {
     }
 
     public void start() throws Exception {
-
+        final EchoServerHandler echoServerHandler = new EchoServerHandler();
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(eventLoopGroup).channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port)).childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(echoServerHandler);
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind().sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            eventLoopGroup.shutdownGracefully().sync();
+        }
     }
 }
