e7bd5a270f46c35499c2d9c51e0b0455f191b8ce
sunnianjun <sunnianjun@huobi.com>
2019-05-21 10:52:00 +0800

    Add: add client start

---
+++ incubator-netty/src/main/java/com/github/taojintianxia/incubator/netty/inaction/chapter2/echo/EchoClient.java
@@ -0,0 +1,46 @@
+package com.github.taojintianxia.incubator.netty.inaction.chapter2.echo;
+
+import com.github.taojintianxia.incubator.netty.inaction.chapter2.echo.handler.EchoClientHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author Nianjun Sun
+ * @date 2019-05-21 10:13
+ */
+public class EchoClient {
+
+    private final String host;
+
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port)).handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(new EchoClientHandler());
+                }
+            });
+            ChannelFuture channelFuture = bootstrap.connect().sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            eventLoopGroup.shutdownGracefully().sync();
+        }
+    }
+}
