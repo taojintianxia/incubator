5ebd5ac605b47f7bdd1807df4d6f7c4e8117ce2d
sunnianjun <sunnianjun@huobi.com>
2019-08-20 16:57:12 +0800

    Add: add configuration annotation

--- incubator-nacos/src/main/java/com/github/taojintianxia/nacos/springboot/config/NacosDynamicConfiguration.java
+++ incubator-nacos/src/main/java/com/github/taojintianxia/nacos/springboot/config/NacosDynamicConfiguration.java
@@ -1,11 +1,15 @@
 package com.github.taojintianxia.nacos.springboot.config;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Nianjun Sun
  * @date 2019-08-20 16:48
  */
+@Data
+@Configuration
 public class NacosDynamicConfiguration {
 
     @Value("${dynamic.student.name:}")
